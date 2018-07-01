b25db018dfb1aab96e5a0d657c9626c47c058f30
Patrick Desjardins <pdesjardins@netflix.com>
2018-06-30 22:33:33 -0700

    Basic Graph in place

--- devpanel/src/Graph.tsx
+++ devpanel/src/Graph.tsx
@@ -1,6 +1,6 @@
 import * as React from "react";
+import { Bar } from "react-chartjs-2";
 import { Statistics } from "./Model";
-
 export interface GraphProps {
     statistics: Statistics;
 }
@@ -10,8 +10,39 @@ export class Graph extends React.Component<GraphProps> {
         super(props);
     }
     public render(): JSX.Element {
+        const data = {
+            labels: ["Mem Read", "Mem Write", "Mem Use", "Db Read", "Db Write", "Db Use", "Http Read", "Http Write", "Http Use"],
+            datasets: [
+                {
+                    label: "Data Access Source",
+                    backgroundColor: "rgba(255,99,132,0.2)",
+                    borderColor: "rgba(255,99,132,1)",
+                    borderWidth: 1,
+                    hoverBackgroundColor: "rgba(255,99,132,0.4)",
+                    hoverBorderColor: "rgba(255,99,132,1)",
+                    data: [
+                        this.props.statistics.readMemoryCount,
+                        this.props.statistics.saveMemoryCount,
+                        this.props.statistics.useMemoryCount,
+                        this.props.statistics.readPersisentCount,
+                        this.props.statistics.savePersistentCount,
+                        this.props.statistics.usePersistentCount,
+                        this.props.statistics.readHttpCount,
+                        this.props.statistics.saveHttpCount,
+                        this.props.statistics.useHttpCount,
+                    ]
+                }
+            ]
+        };
         return <div className="Graph">
-            Graph Here
+            <Bar
+                data={data}
+                width={500}
+                height={200}
+                options={{
+                    maintainAspectRatio: false
+                }}
+            />
         </div>;
     }
 }
